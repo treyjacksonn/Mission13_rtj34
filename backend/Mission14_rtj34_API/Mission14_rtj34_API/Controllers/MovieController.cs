7b97caab590b7cde9b2eb60dae6e368444eb19f2
treyjacksonn <113260616+treyjacksonn@users.noreply.github.com>
2023-04-07 18:31:39 -0700

    API

---
+++ backend/Mission14_rtj34_API/Mission14_rtj34_API/Controllers/MovieController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Mission14_rtj34_API.Data;
+
+namespace Mission14_rtj34_API.Controllers
+{
+    //setup API and path to controller
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        private MovieDbContext context;
+        public MovieController(MovieDbContext temp)
+        {
+            context = temp;
+        }
+
+        public IEnumerable<Movies> Get()
+        {
+            return context.Movies
+                //Filter movies shown
+                .Where(m => m.Edited == "Yes")
+                .OrderBy(m => m.Title)
+                .ToArray();
+        }
+    }
+}
+
---
+++ backend/Mission14_rtj34_API/Mission14_rtj34_API/MovieDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mission14_rtj34_API
+{
+	public class MovieDbContext : DbContext
+	{
+		public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options)
+		{
+		}
+
+	}
+}
+
