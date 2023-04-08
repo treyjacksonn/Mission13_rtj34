cfe02b89082f66dd93b8ba866d48f636c6d7941f
treyjacksonn <113260616+treyjacksonn@users.noreply.github.com>
2023-04-08 01:54:56 -0700

    final commit Mission14_rtj34

--- backend/Mission14_rtj_API/Mission14_rtj_API/Models/Movie.cs
+++ backend/Mission14_rtj_API/Mission14_rtj_API/Models/Movie.cs
@@ -8,12 +8,11 @@ namespace Mission14_rtj_API.Models
         //Creating model based off of database
         [Key]
         public int MovieId { get; set; }
-        public string? Category { get; set; }
 
+        public string? Category { get; set; }
         public string? Title { get; set; }
         public int? Year { get; set; }
         public string? Director { get; set; }
-
         public string? Edited { get; set; }
         public string? LentTo { get; set; }
         public string? Notes { get; set; }
